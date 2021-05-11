6f561d04a8f3df654157008bb39527d0a6166b6e
Halil Demir <m.halildemir94@gmail.com>
2021-05-11 15:59:13 +0200

    tests are implemented for patient controller class

--- clinic/src/test/java/com/bosch/onsite/clinic/controller/PatientControllerTest.java
+++ clinic/src/test/java/com/bosch/onsite/clinic/controller/PatientControllerTest.java
@@ -2,6 +2,7 @@ package com.bosch.onsite.clinic.controller;
 
 import com.bosch.onsite.clinic.domain.Appointment;
 import com.bosch.onsite.clinic.service.DoctorService;
+import com.bosch.onsite.clinic.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,15 +10,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -26,29 +28,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @WebMvcTest
-@ContextConfiguration(classes = {DoctorController.class, DoctorService.class})
-class DoctorControllerTest {
+@ContextConfiguration(classes = {PatientController.class, PatientService.class})
+class PatientControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
     @MockBean
-    private DoctorService doctorService;
+    private PatientService patientService;
 
     @Test
     void getAppointments() throws Exception {
         List<Appointment> appointments = initAppointments(5)
                 .stream()
-                .filter(x -> x.getDoctorId() == 10)
+                .filter(x -> x.getPatientId() == 7)
                 .collect(Collectors.toList());
-        when(doctorService.getAppointments(anyInt())).thenReturn(appointments);
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/doctors/{id}", 10))
+        when(patientService.getAppointments(anyInt())).thenReturn(appointments);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/patients/{id}", 7))
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(jsonPath("$[0].id", is(2)))
-                .andExpect(jsonPath("$[0].doctorId", is(10)))
-                .andExpect(jsonPath("$[0].patientId", is(14)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].patientId", is(7)))
+                .andExpect(jsonPath("$[0].doctorId", is(5)))
                 .andReturn();
-        verify(doctorService, times(1)).getAppointments(anyInt());
+        verify(patientService, times(1)).getAppointments(anyInt());
     }
 
     private List<Appointment> initAppointments(int numberOfAppointment) {
